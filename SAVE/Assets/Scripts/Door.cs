1988b293e9b93c8b22b6ebf617740e1b23136612
Sim Sheng Xue <simshengxue1997@gmail.com>
2019-12-06 17:13:36 +0800

    test

---
+++ SAVE/Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool isOpen;
+    public Collider objCol;
+    public SpriteRenderer objSpr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (isOpen)
+        {
+            objCol.enabled = true;
+            objSpr.enabled = true;
+        } else
+        {
+            objCol.enabled = false;
+            objSpr.enabled = false;
+        }
+    }
+}
