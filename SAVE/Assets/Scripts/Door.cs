8fc0b5a6f6d640f6bc45a3b5dad4c36c0f7eed0d
Sim Sheng Xue <simshengxue1997@gmail.com>
2019-12-06 19:41:03 +0800

    v2

--- SAVE/Assets/Scripts/Door.cs
+++ SAVE/Assets/Scripts/Door.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Door : Activator
 {
+    public bool isOpen;
+    public CameraFollow mainCamera;
     void Start()
     {
-
+        mainCamera = Camera.main.GetComponent<CameraFollow>();
+        if (isOpen)
+        {
+            this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            this.gameObject.GetComponent<Collider>().enabled = false;
+        } else
+        {
+            this.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            this.gameObject.GetComponent<Collider>().enabled = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Trigger()
+    {
+        if (isOpen)
+        {
+            isOpen = false;
+            TriggerOff();
+        } else
+        {
+            isOpen = true;
+            TriggerOn();
+        }
     }
 
     public override void TriggerOn()
