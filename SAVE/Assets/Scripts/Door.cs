4acca997666c13634f9eced29fdc918bf5b85402
alyip98 <alyip98@gmail.com>
2019-12-06 16:07:01 +0800

    Add simple trigger/activator

---
+++ SAVE/Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Activator
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void TriggerOn()
+    {
+        // Hide attached gameObject
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    public override void TriggerOff()
+    {
+        // Hide attached gameObject
+        this.gameObject.GetComponent<MeshRenderer>().enabled = true;
+    }
+}
